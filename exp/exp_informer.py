39375fcd2a58cab6d2af7f6b434a9c7d2c93a7d3
Akashdeepsodhi <97461267+Akashdeepsodhi@users.noreply.github.com>
2025-01-07 15:30:23 +0530

    Update exp_informer.py

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -123,7 +123,7 @@ class Exp_Informer(Exp_Basic):
         self.model.train()
         return total_loss
 
-    def train(self, setting):
+    def train(self, setting, random_var):
         train_data, train_loader = self._get_data(flag = 'train')
         vali_data, vali_loader = self._get_data(flag = 'val')
         test_data, test_loader = self._get_data(flag = 'test')
