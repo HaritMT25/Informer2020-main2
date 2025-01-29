f5a866894478c258151a4965f0859b0dccbc90c5
Harit Tarwani <112678530+HaritMT25@users.noreply.github.com>
2025-01-29 11:35:53 +0530

    Update embed.py

--- models/embed.py
+++ models/embed.py
@@ -81,16 +81,29 @@ class TemporalEmbedding(nn.Module):
                 minute_x).permute(0, 2, 1)  # [batch, d_model, seq_len]
 
 class DataEmbedding(nn.Module):
-    def __init__(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1):
-        super(DataEmbedding, self).__init__()
+    def _init_(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1):
+        super(DataEmbedding, self)._init_()
+
         self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
-        self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type, freq=freq)
+        self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type, freq=freq) if embed_type!='timeF' else TimeFeatureEmbedding(d_model=d_model, embed_type=embed_type, freq=freq)
+
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x, x_mark):
-        # All embeddings now output [batch, d_model, seq_len]
+        
         value_emb = self.value_embedding(x)
-        pos_emb = self.position_embedding(x).permute(0, 2, 1)
+        
+        pos_emb = self.position_embedding(x)
+        
+        # Check if temporal embedding and value embedding have the same size in dim 2
+        # if not, we pad temporal embedding with zeros to match the size of value embedding
+        
         temp_emb = self.temporal_embedding(x_mark)
+
+        if temp_emb.shape[1] != value_emb.shape[1]:
+            # Pad temporal embedding with zeros to match the size of value embedding
+            pad_size = value_emb.shape[1] - temp_emb.shape[1]
+            temp_emb = torch.cat([temp_emb, torch.zeros(temp_emb.shape[0], pad_size, temp_emb.shape[2]).to(temp_emb.device)], dim=1)
+
         return self.dropout(value_emb + pos_emb + temp_emb)
