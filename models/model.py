d77004c6065402ba1d85ee22e987ac2152351670
Harit Tarwani <112678530+HaritMT25@users.noreply.github.com>
2025-02-07 11:40:18 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -68,6 +68,7 @@ class Informer(nn.Module):
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
         enc_out, attns = self.encoder(enc_out, attn_mask=enc_self_mask)
+        print('from enconder embeddigng log', x_enc.shape, flush=True)
 
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
